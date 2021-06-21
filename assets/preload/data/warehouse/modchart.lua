8e56dd2c9cdb3c0dd112b6f4008c680aafa75e6e
craftersshaft <craftersshaftstuff@gmail.com>
2021-06-20 20:32:03 -0400

    kingdom dingdong fart kart

--- assets/preload/data/warehouse/modchart.lua
+++ assets/preload/data/warehouse/modchart.lua
@@ -1,5 +1,7 @@
 function start (song)
 	math.randomseed(os.time())
+	microList = {'warm up!', 'spooky dance', 'help tankman is stuttering', 'stay fresh', 'break hearts'};
+	curSong = 0;
 end
 
 
@@ -8,22 +10,48 @@ function update (elapsed) -- example https://twitter.com/KadeDeveloper/status/13
 end
 
 function beatHit (beat)
-microList = {'warm up!'}
-	if beat % 8 == 0 then
-	newsong = math.random(#microList)	
-    playInst('warehouse/'+tostring(newsong))
-	replaceNoteDataFromJSON('warehouse/'+tostring(newsong))
-    end
-	if beat == 0 then
-   makeText(microList[newsong], "infiniteugh")
-   end
-	if beat % 4 == 0 then
-   destroySprite('infiniteugh')
-   end
+	--if beat % 4 == 0 then
+	--newsong = math.random(#microList)	
+	--newsong = 1
+   -- playInst('warehouse/'+tostring(newsong))
+	--replaceNoteDataFromJSON('warehouse/'+tostring(newsong))
+   -- end
+	--if beat == 0 then
+   --makeText(microList[newsong], "infiniteugh")
+  -- end
+	--if beat % 4 == 0 then
+  -- destroySprite('infiniteugh')
+  -- end
+
+print(beat);
+print(songNotes);
+
+if beat % 4 == 0 then
+  destroySprite('infiniteugh')
+  hasdoneit = 0
+end
 end
 
 function stepHit (step)
-   -- do nothing
+	if step >= (((songNotes)* 4) * 4) then
+	hasdoneit = 1
+	local newsong = math.random(#microList);
+		if (newsong == curSong) then
+		newsong = newsong + 1
+			if (newsong >= microList) then
+			newsong = 0
+			end
+		end
+	print(microList[newsong]);
+	replaceNoteDataFromJSON("warehouse/" .. newsong)
+	if hasdoneit == 1 then
+	makeText(microList[newsong], "infiniteugh")
+	curSong = newsong
+	else
+	makeText('warm up', "infiniteugh")
+	end
+	playInst('warehouse/' .. newsong);
+	end
 end
 
 function keyPressed (key)
